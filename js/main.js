d9a67ea72c785ba8d7aac5f3d5f6301a2db63543
alexag <alxick@gmail.com>
2019-05-25 20:19:37 +0300

    Add mobile styles

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 $( document ).ready(function() {
     console.log('main.js')
+    // var viewportMeta = $('#viewport-meta')
+    //
+    // var viewports = {
+    //     default: viewportMeta.attr('content'),
+    //     landscape: 'width=1024'
+    // };
+    //
+    //  function viewportSet() {
+    //      console.log(screen.width)
+    //     if ( screen.width > 480 ) {
+    //         console.log('if')
+    //         viewportMeta.attr( 'content', viewports.landscape );
+    //     } else {
+    //         viewportMeta.attr( 'content', viewports.default );
+    //     }
+    // }
+    //
+    // viewportSet();
+    //
+    // window.onresize = function() {
+    //     viewportSet();
+    // }
 });
