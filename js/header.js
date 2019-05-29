db7b66c4a6636dceae4365efbd8cab56bd436486
alexag <alxick@gmail.com>
2019-05-29 16:53:02 +0300

    add mobile basket counter

--- js/header.js
+++ js/header.js
@@ -1,20 +1,29 @@
 $( document ).ready(function() {
     var $count = $('.zoomha-info__count')
+    var $mobileCount = $('.zoomha-mobile__basket-count')
 
     function headerBasketCountAdd() {
         $count.addClass('zoomha-info__count_active')
+        $mobileCount.addClass('zoomha-mobile__basket-count_active')
         var count = $count.html() || 0
 
-        $count.html(++count)
+        ++count
+
+        $count.html(count)
+        $mobileCount.html(count)
     }
 
     function headerBasketCountRemove() {
         var count = $count.html()
 
-        $count.html(--count)
+        --count
+
+        $count.html(count)
+        $mobileCount.html(count)
 
         if (count <= 0) {
             $count.removeClass('zoomha-info__count_active')
+            $mobileCount.removeClass('zoomha-mobile__basket-count_active')
         }
     }
 
